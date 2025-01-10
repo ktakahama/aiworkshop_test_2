4d21581c83b87665f80f943b881ccac0e0ec2d7c
Kotaro Takahama <kotaro.takahama@septeni-incubate.co.jp>
2025-01-10 10:38:16 +0900

    tailwind修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-primary': 'linear-gradient(to right, #6366f1, #8b5cf6, #d946ef)',
-      },
-      colors: {
-        'metallic': {
-          light: '#F5F5F7',
-          DEFAULT: '#E2E2E7',
-          dark: '#C5C5C9',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
-} 
+};
